2a3a5e44f4074f0d148d3b4349ba87ae7892e5a1
sz44 <semion44@gmail.com>
2018-09-22 11:13:41 -0700

    added particle class

--- flow_field/sketch.js
+++ flow_field/sketch.js
@@ -1,26 +1,52 @@
 var start = 0;
-var inc = 0.005;
+var inc = 0.05;
 var cols, rows;
-var scl = 40;
+var scl = 20;
+
+zoff = 0;
+
+var fr;
+
+var particles = [];
 
 function setup() {
 	createCanvas(400, 400);
 	cols = floor(width / scl);
 	rows = floor(height / scl);
+	fr = createP('');
+
+	for(let i=0;i<100;i++) {
+		particles[i] = new Particle();
+	}
 }
 
 function draw() {
+	background(255);
 	let yoff = start;
 	for(let y=0;y<rows;y++) {
 		let xoff = start;
 		for(let x=0;x<cols;x++) {
 			var index = (x + y * width) * 4;
-			var r = noise(xoff,yoff) * 255;
-			yoff += inc;
-
-			fill(random(255),random(255),random(255));
-			rect(x * scl, y * scl, scl, scl);
+			var angle = noise(yoff,xoff,zoff) * TWO_PI;
+			var v = p5.Vector.fromAngle(angle);
+			stroke(0);
+			push();
+			translate(x * scl, y * scl);
+			rotate(v.heading());
+			strokeWeight(1);
+			line(0,0,scl,0);
+			pop();
+			xoff += inc;
 		}
-		xoff += inc;
+		yoff += inc;
+		
 	}
+	//zoff += 0.01;
+	for(let i=0;i<particles.length;i++) {
+		particles[i].update();
+		particles[i].show();
+		particles[i].edges();
+	}
+
+	fr.html(floor(frameRate()));
 }
