53b26741315cf65be634d2bc95a7190b95d16cca
sz44 <semion44@gmail.com>
2018-09-25 14:23:03 -0700

    deployed

--- twitter_clone/client/client.js
+++ twitter_clone/client/client.js
@@ -2,9 +2,12 @@ console.log("hello");
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
-const API_URL ='http://localhost:5000/mews';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/mews' : 'https://server-jjnluefzar.now.sh/mews';
+const mewsElement = document.querySelector('.mews');
 
-loadingElement.style.display = 'none';
+loadingElement.style.display = '';
+
+listAllMews();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -30,7 +33,39 @@ form.addEventListener('submit', (event) => {
         console.log(createdMew);
         form.reset();
         loadingElement.style.display = 'none';
-        form.style.display = '';
+        setTimeout(() => {
+          form.style.display = '';
+        }, 10000);
+        listAllMews();
       });
 
 });
+
+function listAllMews() {
+  mewsElement.innerHTML = '';
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(mews => {
+      console.log(mews);
+      mews.reverse();
+      mews.forEach(mew => {
+          const div = document.createElement('div');
+
+          const header = document.createElement('h5');
+          header.textContent = mew.name;
+
+          const contents = document.createElement('p');
+          contents.textContent = mew.content;
+
+          const date = document.createElement('small')
+          date.textContent = new Date(mew.created);
+
+          div.appendChild(header);
+          div.appendChild(contents);
+          div.appendChild(date);
+          mewsElement.appendChild(div);
+      });
+      loadingElement.style.display = 'none';
+
+    });
+}
--- twitter_clone/server/index.js
+++ twitter_clone/server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
+const Filter = require('bad-words');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-const db = monk('localhost/meower');
+const db = monk(process.env.MONGO_URI || 'localhost/meower');
 const mews = db.get('mews');
+
+const filter = new Filter();
 
 app.use(cors());
 app.use(express.json());
+
 
 
 app.get('/', (req, res) => {
@@ -17,16 +22,29 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/mews', (req, res) => {
+  mews
+    .find()
+    .then(mews => {
+      res.json(mews);
+    });
+});
+
 function isValidMew(mew) {
   return mew.name && mew.name.toString().trim() !== '' &&
     mew.content && mew.content.toString().trim() !== '';
 };
 
+app.use(rateLimit({
+  windowMs: 10 * 1000,
+  max: 1
+}));
+
 app.post('/mews', (req, res) => {
   if (isValidMew(req.body)) {
     const mew = {
-      name: req.body.name.toString(),
-      content: req.body.content.toString(),
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
       created: new Date()
     };
 
