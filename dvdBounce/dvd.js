d25f21b6a60c25cc3e8c16b208f0a0093ddf0a87
sz44 <semion44@gmail.com>
2019-02-23 17:25:54 -0800

    dvd image changes color

--- dvdBounce/dvd.js
+++ dvdBounce/dvd.js
@@ -4,26 +4,45 @@ let y;
 let xspeed;
 let yspeed;
 
+let dvd;
+
+let r,g,b;
+
+function preload() {
+  dvd = loadImage("7388.jpg");
+}
+
 function setup() {
   createCanvas(800,600);
-  x = 400;
-  y = 300;
-  xspeed = 10;
-  yspeed = 10;
+  x = random(width-dvd.width);
+  y = random(height-dvd.height);
+  xspeed = 5;
+  yspeed = 5;
+  pickColor();
+}
+
+function pickColor() {
+  r = random(100,256);
+  g = random(100,256);
+  b = random(100,256);
 }
 
 function draw() {
   background(0);
-  rect(x,y,80,60);
+  //  rect(x,y,80,60);
+  tint(r,g,b);
+  image(dvd,x,y);
   x += xspeed;
   y += yspeed;
 
-  if(x == width - 80 || x == 0) {
-    xspeed *= -1;
+  if(x >= width - dvd.width || x <= 0) {
+    xspeed = -xspeed;
+    pickColor();
   }
 
-  if( y == height - 60 || y==0 ) {
-    yspeed *= -1;
+  if( y >= height - dvd.height || y <= 0 ) {
+    yspeed = -yspeed;
+    pickColor();
   }
 
 }
